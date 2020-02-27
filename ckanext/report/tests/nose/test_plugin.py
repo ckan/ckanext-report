8f8b098a0d547aaf48683ad77b3200761a9caebb
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-02-27 21:41:31 +0200

    Init nosetest classes

--- ckanext/report/tests/nose/test_plugin.py
+++ ckanext/report/tests/nose/test_plugin.py
@@ -4,15 +4,18 @@ import ckan.tests.helpers as helpers
 class TestReportPlugin(helpers.FunctionalTestBase):
     @classmethod
     def setup_class(self):
+        super(TestReportPlugin, cls).setup_class()
         if not plugins.plugin_loaded(u'report'):
             plugins.load(u'report')
 
     @classmethod
     def teardown_class(cls):
         plugins.unload(u'report')
+        super(TestReportPlugin, cls).teardown_class()
 
     def test_report_routes(self):
         u"""Test report routes"""
-        res = self.app.get(u'/report')
+        app = self._get_test_app()
+        res = app.get(u'/report')
 
         assert helpers.body_contains(res, u"Reports")
