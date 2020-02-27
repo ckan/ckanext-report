23984a4f585517f1b3804bd8661f79904990afb8
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-02-27 21:25:23 +0200

    Add route tests

--- ckanext/report/tests/nose/test_plugin.py
+++ ckanext/report/tests/nose/test_plugin.py
@@ -1,4 +1,17 @@
-"""Tests for plugin.py."""
+import ckan.plugins as plugins
+import ckan.tests.helpers as helpers
 
-def test_plugin():
-    pass
+class TestReportPlugin(helpers.FunctionalTestBase):
+    @classmethod
+    def setup_class(self):
+            plugins.load(u'report')
+
+    @classmethod
+    def teardown_class(cls):
+        plugins.unload(u'report')
+
+    def test_report_routes(self):
+        u"""Test report routes"""
+        res = self.app.get(u'/report')
+
+        assert helpers.body_contains(res, u"Reports")
--- ckanext/report/tests/test_plugin.py
+++ ckanext/report/tests/test_plugin.py
@@ -1,4 +1,13 @@
-"""Tests for plugin.py."""
+import pytest
 
-def test_plugin():
-    pass
+import ckan.tests.helpers as helpers
+
+@pytest.mark.ckan_config(u'ckan.plugins', u'report')
+@pytest.mark.usefixtures(u'clean_db', u'with_plugins')
+class TestReportPlugin(object):
+
+    def test_report_routes(self, app):
+        u"""Test report routes"""
+        res = app.get(u'/report')
+
+        assert helpers.body_contains(res, u"Reports")
