ffeeabd50e7c869dc8e19a75dcb56b8afa4452d7
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-03-05 21:32:28 +0200

    Fix nav links

--- ckanext/report/plugin/pylons_plugin.py
+++ ckanext/report/plugin/pylons_plugin.py
@@ -9,10 +9,10 @@ class MixinPlugin(p.SingletonPlugin):
 
     def before_map(self, map):
         report_ctlr = 'ckanext.report.controllers:ReportController'
-        map.connect('reports', '/report', controller=report_ctlr,
+        map.connect('report.index', '/report', controller=report_ctlr,
                     action='index')
         map.redirect('/reports', '/report')
-        map.connect('report', '/report/:report_name', controller=report_ctlr,
+        map.connect('report.view', '/report/:report_name', controller=report_ctlr,
                     action='view')
         map.connect('report-org', '/report/:report_name/:organization',
                     controller=report_ctlr, action='view')
