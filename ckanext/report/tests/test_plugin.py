739bbebeb3c2bd0ef46f06ba288459445ddbbd3d
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-03-23 15:35:31 +0200

    Fix pytest plugin configuration

--- ckanext/report/tests/test_plugin.py
+++ ckanext/report/tests/test_plugin.py
@@ -2,7 +2,7 @@ import pytest
 
 import ckan.tests.helpers as helpers
 
-@pytest.mark.ckan_config(u'ckan.plugins', u'report')
+@pytest.mark.ckan_config(u'ckan.plugins', u'report tagless_report')
 @pytest.mark.usefixtures(u'clean_db', u'with_plugins')
 class TestReportPlugin(object):
 
@@ -12,9 +12,9 @@ class TestReportPlugin(object):
 
         assert helpers.body_contains(res, u"Reports")
 
-    @pytest.mark.ckan_config(u'ckan.plugins', u'report tagless_report')
     def test_tagless_report_listed(self, app):
         u"""Test tagless report is listed on report page"""
         res = app.get(u'/report')
 
+        assert helpers.body_contains(res, u'Tagless datasets')
         assert helpers.body_contains(res, u'href="/report/tagless-datasets"')
