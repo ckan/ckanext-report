e13aad6300afeee13143f298e80b9248f66dee53
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-09-20 14:00:54 +0300

    Add Iclick cli

---
+++ ckanext/report/cli.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import click
+from ckanext.report import model as report_model
+
+def get_commands():
+    return [report]
+
+
+@click.group()
+def report():
+    """Generates reports"""
+    pass
+
+@report.command()
+def initdb():
+    """Creates necessary db tables"""
+    report_model.init_tables()
+    click.secho(u'Report table is setup', fg=u"green")
