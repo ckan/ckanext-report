6737f22298a399cda91c453e232623a046dc26c6
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-09-21 10:10:20 +0300

    Add simple tests

--- ckanext/report/tests/test_plugin.py
+++ ckanext/report/tests/test_plugin.py
@@ -1,9 +1,35 @@
 import pytest
 
-from ckan.plugins import plugin_loaded
+from ckan.tests import helpers, factories
 
+import ckanext.report.model as report_model
 
-@pytest.mark.ckan_config('ckan.plugins', 'report')
-@pytest.mark.usefixtures('with_plugins')
-def test_plugin():
-    assert plugin_loaded('report')
+@pytest.fixture
+def report_setup():
+    report_model.init_tables()
+
+@pytest.mark.ckan_config(u'ckan.plugins', u'report tagless_report')
+@pytest.mark.usefixtures(u'clean_db', u'with_plugins', u'report_setup')
+class TestReportPlugin(object):
+
+    def test_report_routes(self, app):
+        u"""Test report routes"""
+        res = app.get(u'/report')
+
+        assert helpers.body_contains(res, u"Reports")
+
+    def test_tagless_report_listed(self, app):
+        u"""Test tagless report is listed on report page"""
+        res = app.get(u'/report')
+
+        assert helpers.body_contains(res, u'Tagless datasets')
+        assert helpers.body_contains(res, u'href="/report/tagless-datasets"')
+
+    def test_tagless_report(self,app):
+        u"""Test tagless report generation"""
+        dataset = factories.Dataset()
+
+        res = app.get(u'/report/tagless-datasets')
+
+        assert helpers.body_contains(res, u"Datasets which have no tags.")
+        assert helpers.body_contains(res, 'href="/dataset/' + dataset['name'] + '"')
