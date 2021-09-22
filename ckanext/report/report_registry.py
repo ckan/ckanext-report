494d79902b6133492dc564751a9a0b8ef059e9fd
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-09-22 16:23:15 +0300

    replace asbool importing from ckan toolkit

--- ckanext/report/report_registry.py
+++ ckanext/report/report_registry.py
@@ -4,7 +4,7 @@ import logging
 import copy
 import re
 
-from paste.deploy.converters import asbool
+from ckan.plugins.toolkit import asbool
 
 from ckan import model
 from collections import OrderedDict
