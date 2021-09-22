59dca5c05c46be1ffd6977d3d6b3d7b2601d8a79
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-09-22 16:14:47 +0300

    Cli working for both forms

--- ckanext/report/cli.py
+++ ckanext/report/cli.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import click
-from ckanext.report import model as report_model
+import ckanext.report.utils as utils
 
 
 def get_commands():
@@ -17,5 +17,28 @@ def report():
 @report.command()
 def initdb():
     """Creates necessary db tables"""
-    report_model.init_tables()
+    utils.initdb()
     click.secho(u'Report table is setup', fg=u"green")
+
+
+@report.command()
+@click.argument(u'report_list', required=False)
+def generate(report_list):
+
+    if report_list:
+        report_list = [s.strip() for s in report_list.split(',')]
+    timings = utils.generate(report_list)
+
+    click.secho(u'Report generation complete %s' % timings, fg=u"green")
+
+
+@report.command()
+def list():
+    utils.list()
+
+
+@report.command()
+@click.argument(u'report_name')
+@click.argument(u'report_options', nargs=-1)
+def generate_for_options(report_name, report_options):
+    utils.generate_for_options(report_name, report_options)
